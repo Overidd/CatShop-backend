7f9413dabca93eadfd8cdf4a9086d378ff03b623
John <johnelvis963@gmail.com>
2024-09-11 21:17:29 -0500

    feat: implementado login view

---
+++ autentication/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.serializers import ModelSerializer
+from profile_client.models import UserClientModel
+from django.contrib.auth.hashers import make_password  # Import para encriptar la contraseña
+
+# Función para crear el token con el correo electrónico añadido al payload
+class UsertokenSerializer():
+   def get_tokens_user(user):
+      refresh = RefreshToken.for_user(user)
+      
+      # Añadir email al token
+      refresh['user_id'] = user.id
+      refresh['email'] = user.email
+      refresh['name'] = user.name
+      
+      return {
+         'refresh': str(refresh),
+         'access': str(refresh.access_token),
+      }
+
+class UserRegisterSerializer(ModelSerializer):
+   class Meta:
+      model = UserClientModel
+      fields = ['name', 'email', 'password']
+
+    # Este método se utiliza para encriptar la contraseña antes de guardarla
+   def validate_password(self, value):
+        return make_password(value)
+   
+class UserEmailSerializer(ModelSerializer):
+    class Meta:
+      model = UserClientModel
+      fields = ['emain', 'verification_code']
+
+class UserLoginSerializer(ModelSerializer):
+   class Meta:
+      model = UserClientModel
+      fields = ['email', 'password']
--- autentication/views.py
+++ autentication/views.py
@@ -1,40 +1,36 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import render
+from rest_framework import serializers, status
 from django.core.mail import send_mail
-from django.contrib.auth.hashers import make_password  # Import para encriptar la contraseña
 
 from profile_client.models import UserClientModel
-from .serializer import UsertokenSerializer
-
+from .serializers import (
+   UsertokenSerializer, UserEmailSerializer, UserRegisterSerializer, UserLoginSerializer
+)
 import random
 
+from django.shortcuts import render
 class UserRegisterView(APIView):
    def post(self, request):
       try: 
-         # Datos requeridos para crear una cuenta
-         name = request.data.get('name')
-         email = request.data.get('email')
-         password = request.data.get('password')
+         serializer = UserRegisterSerializer(data=request.data)
+         serializer.is_valid(raise_exception=True)  # Levanta ValidationError si falla
+         validated_data = serializer.validated_data
 
          # Verificar si el usuario ya esta registrado
+         email = validated_data.validated_data['email']
          if UserClientModel.objects.filter(email=email).exists():
             return Response({
-                "message": "El email ya se encuentra registrado."
+               "message": "El email ya se encuentra registrado."
             }, status=status.HTTP_400_BAD_REQUEST)
 
-         # Crear el codigo de verificacion
-         verification_code = str(random.randint(1000,9999))
-
-         # Encriptar la contraseña antes de almacenarla
-         encrypted_password = make_password(password)
-         print(encrypted_password)
-
-         # Enviar el codigo de verificacion por correo electronico
-         subject = f'Hola {name}, Confirma tu cuenta CatShop'
+         # Crear el código de verificación
+         verification_code = str(random.randint(1000, 9999))
+         
+         # Enviar el código de verificación por correo electrónico
+         subject = f'Hola {validated_data.validated_data["name"]}, Confirma tu cuenta CatShop'
          message = f'Tu código de verificación es: {verification_code}'
-         recipient = [email]
+         recipient = [validated_data.validated_data['email']]
 
          send_mail(
             subject,
@@ -46,15 +42,22 @@ class UserRegisterView(APIView):
          
          # Crear el usuario pero sin verificar
          UserClientModel.objects.create(
-            name=name,
-            email=email,
-            password=encrypted_password,
+            name=validated_data.validated_data['name'],
+            email=validated_data.validated_data['email'],
+            password=validated_data.validated_data['password'],  # Ya encriptada por el serializador
             verification_code=verification_code,
             is_verified=False
          )
+
          return Response({
             "message": "Usuario registrado. Verifica tu email con el código enviado."
             }, status=status.HTTP_201_CREATED)
+      
+      except serializers.ValidationError as e:
+            return Response({
+                "message": "Datos inválidos",
+                "errors": e.detail  # Detalles de los errores de validación
+            }, status=status.HTTP_400_BAD_REQUEST)
       
       except Exception as e:
          return Response({
@@ -67,32 +70,84 @@ class UserRegisterView(APIView):
 class UserRegisterGoogleView(APIView):
    pass
 
-
 class VerifyEmailView(APIView):
+    def post(self, request):
+        try:
+            # Instanciar el serializador con los datos proporcionados
+            serializer = UserEmailSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            validated_data = serializer.validated_data
+
+            # Validar si el email y el código de verificación existen en la base de datos
+            user = UserClientModel.objects.get(
+                email=validated_data.validated_data['email'],
+                verification_code=validated_data.validated_data['verification_code']
+            )
+
+            # Actualizar los campos de verificación de usuario
+            user.is_verified = True
+            user.verification_code = None  # Eliminar el código de verificación
+            user.save()
+
+            # TODO: Generar un token (usando JWT)
+            token = UsertokenSerializer.get_tokens_user(user)
+
+            return Response({
+                "message": "Verificación completada",
+                "access_token": token['access'],
+                "refresh_token": token['refresh']
+            }, status=status.HTTP_200_OK)
+        
+        except serializers.ValidationError as e:
+            # Capturar y manejar los errores de validación del serializador
+            return Response({
+                "message": "Datos inválidos",
+                "errors": e.detail  # Detalles de los errores de validación
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        except UserClientModel.DoesNotExist:
+            # Capturar cuando no se encuentra el usuario en la base de datos
+            return Response({
+                "message": "Código de verificación incorrecto",
+                "error": "error"
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            # Capturar cualquier otro tipo de error inesperado
+            return Response({
+                "message": "Ocurrió un error inesperado",
+                "error": str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+class UserloginView(APIView):
    def post(self, request):
-      email = request.data.get('email')
-      verification_code = request.data.get('verification_code')
       try:
-         # Validar si el email y el codigo de verificacion existe en la base de datos 
-         user = UserClientModel.objects.get(email=email, verification_code=verification_code)
-   
-      except UserClientModel.DoesNotExist:
-          return Response({
-             "message": "Código de incorrecto",
-             "error": "error"
-          }, status=status.HTTP_400_BAD_REQUEST)
+         serializer  = UserLoginSerializer(data=request.data)
+         serializer .is_valid(raise_exception=True)
+         validated_data = serializer.validated_data
+
+         user = UserClientModel.objects.get(email=validated_data["email"])
+
+         if not user.check_password(validated_data["password"]):
+            return Response({
+                "message": "Contraseña incorrecta",
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+         return Response({
+            "message": "Login correcto",
+            "access_token": UsertokenSerializer.get_tokens_user(user)['access'],
+            "refresh_token": UsertokenSerializer.get_tokens_user(user)['refresh']
+            }, status=status.HTTP_200_OK)
       
-      # Actualizar los campos de verificacion de usuario
-      user.is_verified = True
-      user.verification_code = None  # Eliminar el código de verificación
-      user.save()
+      except user.DoesNotExist:
+         return Response({
+            "message": "unauthenticated",
+         }, status=status.HTTP_404_NOT_FOUND)
 
-      #TODO: Generar un token (usando JWT)
-      token = UsertokenSerializer.get_tokens_user(user)
-
-      return Response({
-         "message": "Verificación completada",
-         "access_token": token['access'],
-         "refresh_token": token['refresh']
-      }, status=status.HTTP_200_OK)
- 
+      except Exception as e:
+         return Response({
+            "message": "Ocurrió un error inesperado",
+            "error": str(e)
+         }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
