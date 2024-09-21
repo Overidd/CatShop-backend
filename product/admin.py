6634a3f7866529ae435399bd958ecda30113950a
John <johnelvis963@gmail.com>
2024-09-20 20:28:06 -0500

    fix:

--- product/admin.py
+++ product/admin.py
@@ -10,20 +10,20 @@ from .models import (
 # Inline para agregar detalles del producto dentro del formulario de producto
 class ProductDetailInline(admin.StackedInline):
     model = ProductDetailModel
-    extra = 1  # Permite agregar un solo formulario de detalles
+    extra = 1  
 
 # Inline para agregar imágenes del producto dentro del formulario de producto
 class ProductImageInline(admin.TabularInline):
     model = ProductImageModel
-    extra = 1  # Permite agregar múltiples imágenes
+    extra = 1  
 
 # Personalización del admin para ProductModel
 @admin.register(ProductModel)
 class ProductAdmin(admin.ModelAdmin):
-   list_display = ('name', 'price', 'stock', 'status', 'category', 'brand')
+   list_display = ('name', 'price', 'discount','stock', 'category', 'brand', 'status')
    search_fields = ('name', 'code')
    list_filter = ('category', 'brand', 'status')
-   inlines = [ProductDetailInline, ProductImageInline]  # Incluir los inlines para detalles e imágenes
+   inlines = [ProductDetailInline, ProductImageInline] 
    fieldsets = (
         ('Información del Producto', {
             'fields': ('name', 'price', 'discount', 'description',  'status', 'stock', 'category', 'brand')
